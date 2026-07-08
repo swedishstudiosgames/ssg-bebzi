ae21971eb7fd73b40dcee61486958fa427216826
Lukas A. <lukas.akdogan@gmail.com>
2026-07-08 13:22:36 +0200

    Add files via upload
    Signed-off-by: Lukas A. <lukas.akdogan@gmail.com>

---
+++ gemini-code-1783509681738.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        bebzi: {
+          red: '#E60000',
+          dark: '#111111',
+          accent: '#FF3333'
+        }
+      }
+    }
+  },
+  plugins: [],
+}
